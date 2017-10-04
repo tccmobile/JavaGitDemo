cf927e4296c51779f85b6fc36dba2838751a40d2
tccmobile <drwillsmith@gmail.com>
2017-10-04 13:33:35 -0500

    Initial commit

---
+++ src/AuthoringAssistant.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+
+public class AuthoringAssistant {
+
+    public static char printMenu(Scanner scnr){
+
+
+        return 'q';
+    }
+
+    public static int getNumOfNonWSCharacters(String usrStr){
+
+        return 0;
+    }
+
+    public static int getNumOfWords(String usrStr){
+
+        return 0;
+    }
+
+    public static String replaceExclamation(String usrStr){
+
+        return null;
+    }
+
+    public static String shortenSpace(String usrStr){
+
+        return null;
+    }
+
+    public static void main(String[] args) {
+      /* Type your code here. */
+        Scanner scnr = new Scanner(System.in);
+        String usrStr;
+        char choice='?';
+
+        System.out.println("Enter a sample text:");
+        usrStr = scnr.nextLine();
+        System.out.println();
+        System.out.println("You entered: "+usrStr);
+
+        do{
+            // Call printMenu
+            // Execute method based on choice returned
+
+        } while (choice!='q');
+
+
+        return;
+    }
+}
